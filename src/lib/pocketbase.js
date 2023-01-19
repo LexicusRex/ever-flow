970e9b8c8f600af7386cded407f1d8ba65085e6b
Alex <47023455+LexicusRex@users.noreply.github.com>
2023-01-19 14:22:56 +1100

    chore: add housekeeping env variables and imports

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -1,7 +1,7 @@
 // JavaScript SDK
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase("http://127.0.0.1:8090");
+const pb = new PocketBase(process.env.REACT_APP_PB_URL);
 export default pb;
 /* 
 ...
