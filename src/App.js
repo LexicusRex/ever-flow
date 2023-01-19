eed2e7be724341bb8db84ff6e11a950c064f194d
Alex <47023455+LexicusRex@users.noreply.github.com>
2023-01-19 14:25:53 +1100

    refactor: add bootstrap import for quick styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 // function App() {
 //     return (
@@ -25,7 +26,12 @@ import LoginPage from "./pages/LoginPage";
 function App() {
     return (
         <div>
-            <LoginPage />
+            <link
+                rel="stylesheet"
+                href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+                crossorigin="anonymous"></link>
+            <SignupPage />
         </div>
     );
 }
