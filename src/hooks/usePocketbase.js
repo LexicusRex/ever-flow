bdbd3d7e6d76addfd1b44dd73ebd4239f74938ba
Alex <47023455+LexicusRex@users.noreply.github.com>
2023-01-20 13:20:32 +1100

    feat: update pocketbase data

--- src/hooks/usePocketbase.js
+++ src/hooks/usePocketbase.js
@@ -4,7 +4,7 @@ export const usePocketbase = () => {
     const pb = new PocketBase(process.env.REACT_APP_PB_URL);
 
     const getGroups = async () => {
-        return pb.collection("messages").getFullList(200, { sort: "-created" });
+        return pb.collection("groups").getFullList(200, { sort: "-created" });
     };
 
     const getUsers = async () => {
