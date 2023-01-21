0efaa1cd7596f100a6308af03f31de4d6c311106
Sam Yu <samyulive@gmail.com>
2023-01-21 22:30:58 +1100

    feat: add boot-strap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // Page imports
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 
 function App() {
-    return (
-        <div>
-            <Router>
-                <Routes>
-                    <Route exact path="/" element={<LoginPage/>}/>
-                    <Route exact path="/login" element={<LoginPage/>}/>
-                    <Route exact path="/register" element={<SignupPage/>} />
-                </Routes>
-            </Router>
-        </div>
-    );
+  return (
+    <div>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossOrigin="anonymous"
+      ></link>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<LoginPage />} />
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route exact path="/register" element={<SignupPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
