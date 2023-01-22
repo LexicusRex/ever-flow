bc3b9c03e045f0a97958a7eabb59c3224298c588
Sam Yu <samyulive@gmail.com>
2023-01-22 15:15:24 +1100

    feat: color change for status

--- src/components/TaskInfo.js
+++ src/components/TaskInfo.js
@@ -1,28 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function TaskInfo(props) {
+    let start = new Date(
+        props.info.start_date.slice(0, 10).replaceAll("-", "/")
+    );
+    let due = new Date(props.info.due_date.slice(0, 10).replaceAll("-", "/"));
+
+    const status = "Complete";
+
+    const colorMap = {
+        Assigned: "text-yellow-500",
+        Ongoing: "text-orange-500",
+        Overdue: "text-red-500",
+        Complete: "text-green-500",
+    };
     return (
         <div className="flex flex-row text-[#414141] mb-[1.9rem] font-normal">
             <p className="absolute text-[1.2rem] ml-[2.3rem]">
-                {props.courseID}
+                {/* {props.courseID} */}
+                {"ACTL3182"}
             </p>
             <p className="absolute text-[1.2rem] ml-[12.15rem]">
-                {props.projectName}
+                {/* {props.projectName} */}
+                {"Assignment A"}
             </p>
             <p className="absolute text-[1.2rem] ml-[27.3rem]">
-                {props.taskName}
+                {props.info.title}
             </p>
-            <p className="absolute text-[1.2rem] ml-[60.1rem]">
-                {props.startDate}
-            </p>
+            <p className="absolute text-[1.2rem] ml-[60.1rem] ">{`${start.getDate()}/${(
+                "0" +
+                (start.getMonth() + 1)
+            ).slice(-2)}/${start.getFullYear()}`}</p>
             <p className="absolute text-[1.2rem] ml-[74.6rem]">
-                {props.dueDate}
+                {`${due.getDate()}/${("0" + (due.getMonth() + 1)).slice(
+                    -2
+                )}/${due.getFullYear()}`}
             </p>
-            <p className="absolute text-[1.2rem] ml-[89.05rem]">
-                {props.status}
+            <p
+                className={`absolute text-[1.2rem] ml-[89.05rem] ${colorMap[status]}`}
+            >
+                {/* {props.status} */}
+                {status}
             </p>
             <p className="absolute text-[1.2rem] ml-[105rem]">
-                {props.progress}
+                {/* {props.progress} */} asdasda
             </p>
         </div>
     );
