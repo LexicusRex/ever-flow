33987d539dee693a6b013b1ec51ad3dda2c64199
Alex <47023455+LexicusRex@users.noreply.github.com>
2023-01-25 17:43:53 +1100

    feat: add global accent colors for tailwind & CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    mode: 'jit',
+    mode: "jit",
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {
             width: {
-                'screen-1/2': '50vw'
-            }
+                "screen-1/2": "50vw",
+            },
+            colors: {
+                "full-accent": "var(--accent-color)",
+                "mild-accent": "var(--accent-mild-color)",
+                "focus-accent": "var(--accent-focus-color)",
+                "semi-accent": "var(--accent-semi-color)",
+            },
         },
     },
     plugins: [],
