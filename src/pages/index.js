2a23509c0fd0c1d52a4dc3c93a10c3ee19eb0469
Alex <47023455+LexicusRex@users.noreply.github.com>
2023-01-26 20:04:33 +1100

    feat: export projects page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as LoginPage } from "./LoginPage";
 export { default as SignupPage } from "./SignupPage";
 export { default as Dashboard } from "./Dashboard";
+export { default as Projects } from "./Projects";
