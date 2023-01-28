c821d3861a665d4fcd892c96a0b5c89abe0dcdc3
Alex <47023455+LexicusRex@users.noreply.github.com>
2023-01-28 22:42:34 +1100

    refactor: add responsiveness to task info

--- src/components/NewTaskInfo.js
+++ src/components/NewTaskInfo.js
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import "../assets/styles/Task.css";
 
 function NewTaskInfo({ course, project, task, start, due, status, progress }) {
     const statusColors = {
@@ -21,18 +22,25 @@ function NewTaskInfo({ course, project, task, start, due, status, progress }) {
                 "hover:bg-mild-accent"
             )}
         >
-            <div className={`w-[6rem] ${taskInfoStyles}`}>{course}</div>
-            <div className={`w-[11rem]  ${taskInfoStyles}`}>{project}</div>
-            <div className={`w-[25rem]  ${taskInfoStyles}`}>{task}</div>
-            <div className={`w-[6.4rem]`}>{start}</div>
+            <div id="task-view" className={`w-[6rem] ${taskInfoStyles}`}>
+                {course}
+            </div>
+            <div className={`w-[15rem]  ${taskInfoStyles}`}>{project}</div>
+            <div className={`w-[30rem] ${taskInfoStyles}`}>{task}</div>
+            <div id="task-view-a" className={`w-[6.4rem]`}>
+                {start}
+            </div>
             <div className={`w-[6.4rem]`}>{due}</div>
             <div className={`w-[6rem] ${statusColors[status]}`}>{status}</div>
             <div
                 id="taskInfoProgressBar"
                 className={clsx(
                     "h-[1.3rem]",
-                    "min-w-[10rem]",
-                    "max-w-[14rem]",
+                    "w-[11rem]",
+                    "hidden",
+                    "2xl:block",
+                    // "min-w-[10rem]",
+                    // "max-w-[14rem]",
                     "rounded-[0.2rem]",
                     "bg-[#D9D9D9]"
                 )}
