5d7c9e4c1a59d277242dbfe78cd611dd26052afb
Alex <47023455+LexicusRex@users.noreply.github.com>
2023-01-30 15:10:35 +1100

    fix: align welcome message to left

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,8 +5,10 @@ import TaskView from "components/TaskView";
 import NewTaskView from "components/NewTaskView";
 import MenuBar from "components/MenuBar";
 import TaskOverview from "components/TaskOverview";
-import "react-modern-calendar-datepicker/lib/DatePicker.css";
-import { Calendar } from "react-modern-calendar-datepicker";
+// import "react-modern-calendar-datepicker/lib/DatePicker.css";
+import "@hassanmojab/react-modern-calendar-datepicker/lib/DatePicker.css";
+// import { Calendar } from "react-modern-calendar-datepicker";
+import { Calendar } from "@hassanmojab/react-modern-calendar-datepicker";
 import MountainProgressView from "components/MountainProgressView";
 import "../assets/styles/DashBoard.css";
 import LoginPage from "./LoginPage";
@@ -31,17 +33,17 @@ function Dashboard() {
             {/* <div className="absolute left-[245px]"> */}
             <MenuBar itemFocus={"Dashboard"} />
             <div className="h-screen min-w-[32rem]"></div>
-            <div className="max-w-[170rem] pl-[1.5rem]">
+            <div className="max-w-[170rem] px-[1.5rem]">
                 <div className="flex h-[12.5rem] w-full items-end  px-[2rem]">
-                    <div className="flex items-center">
+                    <div className="flex items-center py-4">
                         <div className="h-[6rem] w-[6rem] rounded-full bg-yellow-300"></div>
-                        <div className="px-[2rem] ">
-                            <h1 className="text-[3.2rem] font-semibold leading-[2.6rem]">
+                        <div className="px-[2rem] text-left">
+                            <h1 className="m-0 text-[3.2rem] font-semibold leading-[2.6rem]">
                                 Welcome back{" "}
                                 {pb.authStore.isValid &&
                                     `${pb.authStore.model.name.split(" ")[0]}!`}
                             </h1>
-                            <p className="pt-[1rem] text-[2rem]">
+                            <p className="m-0 pt-[1rem] text-[2rem]">
                                 {new Date(Date.now()).toLocaleDateString(
                                     "en-GB",
                                     {
@@ -65,13 +67,13 @@ function Dashboard() {
                 <div className="mb-[3rem] mt-[1.5rem] grid grid-flow-col gap-[4rem]">
                     <TaskOverview />
                     <div className="h-full w-full rounded-[10px] bg-white shadow">
-                        {/* <Calendar
+                        <Calendar
                             value={selectedDay}
                             onChange={setSelectedDay}
                             inputPlaceholder="Select a date" // placeholder
                             formatInputText={formatInputValue} // format value
                             shouldHighlightWeekends
-                        /> */}
+                        />
                     </div>
                 </div>
                 <div className="mt-[3rem] grid grid-flow-col gap-[4rem]">
