c1681ca7fe81885fdc182158a0090e5d269dc603
Alex <47023455+LexicusRex@users.noreply.github.com>
2023-01-30 13:05:41 +1100

    refactor: update to use Outlet

--- src/components/authentication/ProtectedRoute.js
+++ src/components/authentication/ProtectedRoute.js
@@ -1,20 +1,30 @@
-import React from "react";
-import { Navigate, useLocation } from "react-router-dom";
+import { Navigate, useLocation, Outlet } from "react-router-dom";
+import useAuth from "hooks/useAuth";
 import pb from "lib/pocketbase";
-import { Dashboard } from "pages";
+// export default function ProtectedRoute({ page }) {
+//     let location = useLocation();
+//     if (!pb.authStore.isValid) {
+//         return (
+//             <Navigate
+//                 to={{ pathname: "/", state: { from: location } }}
+//                 replace
+//             />
+//         );
+//     }
+//     console.log("AIOSUDHOIASHD");
+//     return page;
+//     // Not returning the correct page
+//     // return <Dashboard />;
+// }
 
-export default function ProtectedRoute({ page }) {
-    let location = useLocation();
-    if (!pb.authStore.isValid) {
-        return (
-            <Navigate
-                to={{ pathname: "/", state: { from: location } }}
-                replace
-            />
-        );
-    }
-    console.log("AIOSUDHOIASHD");
-    return page;
-    // Not returning the correct page
-    // return <Dashboard />;
-}
+const ProtectedRoute = () => {
+    const { auth } = useAuth();
+    const location = useLocation();
+
+    return pb.authStore.isValid ? (
+        <Outlet />
+    ) : (
+        <Navigate to="/login" state={{ from: location }} replace />
+    );
+};
+export default ProtectedRoute;
