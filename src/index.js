f282d2bdc8602fb32e30f7d644d5c518f9f5367a
Alex <47023455+LexicusRex@users.noreply.github.com>
2023-01-30 12:58:31 +1100

    feat: add routing to index

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { QueryClientProvider, QueryClient } from "react-query";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
-            <App />
+            <Router>
+                <Routes>
+                    <Route path="/*" element={<App />} />
+                </Routes>
+            </Router>
         </QueryClientProvider>
     </React.StrictMode>
 );
