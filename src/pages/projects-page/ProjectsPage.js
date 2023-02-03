2972c25c3ab5e07b979f735d7753e2e8cca5b002
Alex <47023455+LexicusRex@users.noreply.github.com>
2023-02-03 14:47:35 +1100

    feat: restructure projects page components

--- src/pages/projects-page/ProjectsPage.js
+++ src/pages/projects-page/ProjectsPage.js
@@ -1,11 +1,11 @@
-import ProjectTaskBoard from "components/project-board/ProjectTaskBoard";
+import ProjectTaskBoard from "pages/projects-page/components/ProjectTaskBoard";
 import MenuBar from "components/MenuBar";
 
 function Projects() {
     return (
         <div className="flex h-screen">
             <MenuBar itemFocus={"Projects"} />
-            <div className="h-screen min-w-[32rem]"></div>
+            <div className="h-screen min-w-[28rem]"></div>
             <ProjectTaskBoard />
         </div>
     );
