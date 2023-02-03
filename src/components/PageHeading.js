ddc475f236743c1d5e3bf4bad6cc0f65a5f6d997
Alex <47023455+LexicusRex@users.noreply.github.com>
2023-02-03 22:44:27 +1100

    feat: add a toggle for header title

--- src/components/PageHeading.js
+++ src/components/PageHeading.js
@@ -1,9 +1,11 @@
 import SearchBox from "./SearchBox";
 
-const PageHeader = ({ pageTitle }) => {
+const PageHeader = ({ hasMyHeader, pageTitle }) => {
     return (
-        <div className="flex w-full items-center justify-between px-[4.7rem] pt-[7.5rem]">
-            <h1 className="text-[3rem] font-semibold">My {pageTitle}</h1>
+        <div className="flex w-full items-center justify-between px-[4.5rem] pt-[7.5rem]">
+            <h1 className="text-[3rem] font-semibold">
+                {hasMyHeader && "My"} {pageTitle}
+            </h1>
             <div className="flex items-center">
                 <SearchBox />
                 <div className="h-[40px] w-[40px] rounded-full bg-full-accent" />
