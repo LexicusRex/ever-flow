73da7c1639c73ab91594acb72afc76b5989706d3
Alex <47023455+LexicusRex@users.noreply.github.com>
2023-02-13 16:52:48 +1100

    fix: replace original environment socket address

--- src/hooks/usePocketbase.js
+++ src/hooks/usePocketbase.js
@@ -1,7 +1,7 @@
 import PocketBase from "pocketbase";
 
 export const usePocketbase = () => {
-    const pb = new PocketBase(process.env.REACT_APP_PB_URL);
+    const pb = new PocketBase("http://127.0.0.1:8090");
     const getUsers = async () => {
         return pb.collection("users").getFullList(200, {
             sort: "-created",
